2d2ba6b09a22a450a73fdfcce49b18b5383bc9c8
Greg Markou <gregorymarkou@gmail.com>
2016-05-26 11:32:06 -0400

    Add scroll lock feature

---
+++ content.js
@@ -0,0 +1,18 @@
+$('html, body').css({
+    'overflow': 'hidden',
+    'height': '100%'
+});
+
+// var window.restore = function(){
+//   $('html, body').css({
+//     'overflow': 'auto',
+//     'height': 'auto'
+//   });
+// }
+// //
+// // var window.enable = function(){
+// //   $('html, body').css({
+// //       'overflow': 'hidden',
+// //       'height': '100%'
+// //   });
+// // }
