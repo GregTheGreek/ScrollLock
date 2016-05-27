7bd9f2e87a29c72d1c9b4a28bc951d80205371b1
Greg Markou <gregorymarkou@gmail.com>
2016-05-26 23:03:16 -0400

    Upload to chrome store Version 0.0.1

--- content.js
+++ content.js
@@ -2,17 +2,3 @@ $('html, body').css({
     'overflow': 'hidden',
     'height': '100%'
 });
-
-// var window.restore = function(){
-//   $('html, body').css({
-//     'overflow': 'auto',
-//     'height': 'auto'
-//   });
-// }
-// //
-// // var window.enable = function(){
-// //   $('html, body').css({
-// //       'overflow': 'hidden',
-// //       'height': '100%'
-// //   });
-// // }
